05a78b4882b8f0591f1f8f5ed2b1e9a3961067d8
UnFik <fikriilhamarifin_1313621027@mhs.unj.ac.id>
2023-10-17 10:55:41 +0700

    add Column to Main Activity

--- app/src/main/java/com/example/uts_mobcom/MainActivity.kt
+++ app/src/main/java/com/example/uts_mobcom/MainActivity.kt
@@ -47,7 +47,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyVerticalGridDemo()
+                    Column {
+                        HeaderTitle(title = "Aplikasi List Mata Kuliah")
+                        LazyVerticalGridDemo()
+                    }
                 }
             }
         }
